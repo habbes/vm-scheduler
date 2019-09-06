8a0e30e66676c313752e1cfd45f1e1696db2c7dd
Clément Habinshuti <hello@habbes.xyz>
2019-09-06 11:28:50 +0300

    Get guest CPU time

--- CPU/vcpu_scheduler.c
+++ CPU/vcpu_scheduler.c
@@ -80,12 +80,39 @@ int guestListIdAt(GuestList *gl, int i)
     return gl->ids[i];
 }
 
+
+void printParamValue(virTypedParameterPtr param)
+{
+    switch(param->type) {
+        case VIR_TYPED_PARAM_INT:
+            printf("%d", param->value.i);
+            break;
+        case VIR_TYPED_PARAM_LLONG:
+            printf("%lld", param->value.l);
+            break;
+        case VIR_TYPED_PARAM_ULLONG:
+            printf("%llu", param->value.l);
+            break;
+        case VIR_TYPED_PARAM_STRING:
+            printf("%s", param->value.s);
+            break;
+        case VIR_TYPED_PARAM_DOUBLE:
+            printf("%.2f", param->value.d);
+            break;
+        default:
+            printf("%llu", param->value);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     char * uri = "qemu:///system";
     virConnectPtr conn = NULL;
+    virNodeInfo nodeInfo;
     GuestList *guestList = NULL;
     virDomainInfo domainInfo;
+    virDomainPtr domain;
+    virTypedParameterPtr params;
     int i = 0;
 
     conn = virConnectOpen(uri);
@@ -95,14 +122,29 @@ int main(int argc, char *argv[])
     check(guestList, "Failed to create guest list");
 
     for (i = 0; i < guestList->count; i++) {
-        virDomainGetInfo(guestListDomainAt(guestList, i), &domainInfo);
-        printf("Guest OS Id: %d\n", guestListIdAt(guestList, i));
+        domain = guestListDomainAt(guestList, i);
+        virDomainGetInfo(domain, &domainInfo);
+        printf("Guest VM Id: %d\n", guestListIdAt(guestList, i));
         printf("- state: %u\n", domainInfo.state);
         printf("- max mem: %lu\n", domainInfo.maxMem);
         printf("- mem: %lu\n", domainInfo.memory);
         printf("- num vcpus: %u\n", domainInfo.nrVirtCpu);
-        printf("- cpu tume: %lld\n", domainInfo.cpuTime);
+        printf("- cpu time: %lld\n", domainInfo.cpuTime);
+
+        int ncpus = virDomainGetCPUStats(domain, NULL, 0, 0, 0, 0);
+        int nparams = virDomainGetCPUStats(domain, NULL, 0, 0, 1, 0);
+        params = calloc(ncpus * nparams, sizeof(virTypedParameter));
+        virDomainGetCPUStats(domain, params, nparams, 0, ncpus, 0);
+        for (int c = 0; c < ncpus; c++) {
+            printf("-- cpu %d\n", c);
+            for (int p = 0; p < nparams; p++) {
+                printf("--- param %s type %d value ", params[p].field, params[p].type);
+                printParamValue(params + (nparams * c + p));
+                printf("\n");
+            }
+        }
         puts("");
+        free(params);
     }
 
     guestListFree(guestList);
